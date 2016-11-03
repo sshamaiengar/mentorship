6530a5f86fa7a4a254a1e19dd2b504c9d8d3018e
Stephen Shamaiengar <wizard@math-io.com>
2016-11-02 21:58:20 -0400

    [ADD] Starting work with chi square analysis of data. chi_square.py displays a chi square distribution based on epsilon and reduced cross section from 8 and 1.6 GeV data [FIX] Changed rosenbluth.py to be modular, i.e. its functions can be imported without it running normally.

--- chi_square.py
+++ chi_square.py
@@ -1,4 +1,85 @@
-# write down form factor means and uncertainties (sigma from fit) in table
-# rows are Q^2
+# -*- coding: utf-8 -*-
 
-# add 1.6 GeV spectrometer data to data.csv and make a new table
+from numpy import deg2rad, linalg, tan, sin, cos, random, array, std, mean, where
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import scipy.stats as stats
+import sys
+import csv
+from rosenbluth import rosenbluth, partition
+
+#x = epsilon, y = reduced, fit_function = lambda x: a*x + b
+def chi_square(x, y, error, fit_function):
+	vals = []
+	for i in range(len(x)):
+		vals.append((y[i] - fit_function(x[i]))**2/error[i])
+	return sum(vals)
+
+if len(sys.argv) > 1:
+	q_squared = []
+	energies = []
+	thetas = []
+	cross_sections = []
+	uncertainties = []
+
+	file = sys.argv[1]
+	try:
+		with open(file, 'rb') as f:
+			data = csv.reader(f, delimiter=" ")
+			# skip header row
+			next(data)
+			for row in data:
+				q_squared.append(float(row[0]))
+				energies.append(float(row[1]))
+				thetas.append(float(row[2]))
+				cross_sections.append(float(row[3]))
+				uncertainties.append(float(row[4]))
+	except IOError as e:
+		print(e)
+else:
+	q_squared = [4.0,4.0,4.0,4.0,4.0]
+	energies = [3.4, 3.956, 4.507, 5.507, 9.8]
+	thetas = [57.572, 43.707, 35.592, 26.823,13.248]
+	cross_sections = [1.297e-2, 2.77e-2, 4.929e-2, 1.023e-1, 6.18e-1]
+	uncertainties = [2.243e-4,4.407e-4,7.853e-4,1.370e-3,8.073e-3]
+
+q_squared, energies, thetas, cross_sections, total_errors = partition(q_squared, energies, thetas, cross_sections, uncertainties)
+eps = []
+red = []
+chi_squares = []
+samples = 10000
+
+#just do one Q^2 for now
+for i in range(len(q_squared[0])):
+	q2 = q_squared[0][i]
+	energy = energies[0][i]
+	theta = thetas[0][i]
+	cross_section = cross_sections[0][i]
+	error = total_errors[0][i]
+
+	epsilon, tau, reduced = rosenbluth(q2, energy, theta, cross_section)
+
+	eps.append([epsilon]*samples)
+	red.append(random.normal(reduced, 0.02, samples))
+
+eps = array(eps)
+red = array(red)
+
+for i in range(samples):
+
+	eps_samples = eps[:,i]
+	red_samples = red[:,i]
+	print(eps_samples)
+	reg = stats.linregress(eps_samples, red_samples)
+	chi_squares.append(chi_square(eps_samples, red_samples, total_errors[0], lambda x: reg[0]*x+reg[1]))
+
+#plot chi square distribution
+plt.delaxes()
+plt.figure(figsize=(12,9))
+ax = plt.subplot(111)
+ax.set_xlabel(r'$\chi^2$', fontsize=30)
+ax.set_ylabel(r'Frequency', fontsize=30)
+plt.hist(chi_squares, bins=100, normed=1, facecolor='gray', alpha=0.5, linewidth=2, histtype="stepfilled")
+
+plt.show()
+
--- rosenbluth.py
+++ rosenbluth.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from numpy import deg2rad, linalg, tan, sin, cos, random, array, std, mean
+from numpy import deg2rad, linalg, tan, sin, cos, random, array, std, mean, where
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import scipy.stats as stats
 import sys
 import csv
+
+
+# constants
+fsc = 0.00729735256
+mass = 0.938 # GeV
 
 # returns epsilon, tau, and reduced cross section (can be used to calculate form factors with linear fit)
 def rosenbluth(q_squared, energy, theta, cross_section):
@@ -68,10 +73,10 @@ def plot_form_factors(ge2_vals, gm2_vals, q2):
 
 	n, bins, patches = plt.hist(ge2_vals, bins=50, normed=1, facecolor='red', linewidth=3, histtype="stepfilled")
 	y = mlab.normpdf(bins, mu1, sigma1)
-	plt.plot(bins, y, "-", color="orange", linewidth=4)
-	plt.axvspan(mu1, mu1+sigma1, color="white", alpha=0.5)
-	plt.xticks(fontsize=14)
-	plt.yticks(fontsize=14)
+	plt.plot(bins, y, "-", color="black", linewidth=4)
+	plt.axvspan(mu1-sigma1, mu1+sigma1, color="white", alpha=0.5)
+	plt.xticks(fontsize=20)
+	plt.yticks(fontsize=20)
 
 	# ax2 = f1.add_subplot(122)
 	ax2 = plt.subplot(122)
@@ -79,147 +84,148 @@ def plot_form_factors(ge2_vals, gm2_vals, q2):
 	mu2, sigma2 = stats.norm.fit(gm2_vals)
 	n, bins, patches = plt.hist(gm2_vals, bins=50, normed=1, facecolor='blue', linewidth=3, histtype="stepfilled")
 	y = mlab.normpdf(bins, mu2, sigma2)
-	plt.plot(bins, y, "-", color="orange", linewidth=4)
-	plt.axvspan(mu2, mu2+sigma1, color="white", alpha=0.5)
-	plt.xticks(fontsize=14)
-	plt.yticks(fontsize=14)
+	plt.plot(bins, y, "-", color="black", linewidth=4)
+	plt.axvspan(mu2-sigma2, mu2+sigma2, color="white", alpha=0.5)
+	plt.xticks(fontsize=20)
+	plt.yticks(fontsize=20)
 
-	# ax1.set_title(r'$\mathrm{Histogram\ of\ G_E^2: Q^2 = %.3f},\ \mu = %f,\ \sigma = %f$' % (q2,mu1,sigma1), fontsize=20)
-	ax1.set_xlabel(r'$G_E^2$', fontsize=20)
-	ax1.set_ylabel(r'Frequency', fontsize=20)
-	ax1.annotate('$Q^2 = %f$ \n $\mu = %f$ \n $\sigma = %f$' % (q2, mu1, sigma1), xy=(mu1, 0), xycoords='data',
-		xytext=(0.7, 0.8), textcoords="axes fraction", verticalalignment='bottom', horizontalalignment='left', fontsize=20)
-	# ax2.set_title(r'$\mathrm{Histogram\ of\ G_M^2: Q^2 = %.3f},\ \mu = %f,\ \sigma = %f$' % (q2,mu2,sigma2), fontsize=20)
-	ax2.set_xlabel(r'$G_E^2$', fontsize=20)
-	ax2.set_ylabel(r'Frequency', fontsize=20)
-	ax2.annotate('$Q^2 = %f$ \n $\mu = %f$ \n $\sigma = %f$' % (q2, mu2, sigma2), xy=(mu2, 0), xycoords='data',
-		xytext=(0.7, 0.8), textcoords="axes fraction", verticalalignment='bottom', horizontalalignment='left', fontsize=20)
+	ax1.set_xlabel(r'$G_E^2$', fontsize=30)
+	ax1.set_ylabel(r'Frequency', fontsize=30)
+	ax1.annotate('$Q^2 = %.3f$ \n $\mu = %f$ \n $\sigma = %f$' % (q2, mu1, sigma1), xy=(mu1, 0), xycoords='data',
+		xytext=(0.6, 0.7), textcoords="axes fraction", verticalalignment='bottom', horizontalalignment='left', fontsize=30)
+	# ax1.annotate('$2\sigma$', xy=(mu1, ax1.get_ylim()[1]/3), xycoords='data', xytext=(mu1, ax1.get_ylim()[1]/3), textcoords='data', horizontalalignment='center', fontsize=30)
+	ax2.set_xlabel(r'$G_M^2$', fontsize=30)
+	ax2.set_ylabel(r'Frequency', fontsize=30)
+	ax2.annotate('$Q^2 = %.3f$ \n $\mu = %f$ \n $\sigma = %f$' % (q2, mu2, sigma2), xy=(mu2, 0), xycoords='data',
+		xytext=(0.6, 0.7), textcoords="axes fraction", verticalalignment='bottom', horizontalalignment='left', fontsize=30)
+	# ax2.annotate('$2\sigma$', xy=(mu2, ax2.get_ylim()[1]/3), xycoords='data', xytext=(mu2, ax2.get_ylim()[1]/3), textcoords='data', horizontalalignment='center', fontsize=30)
 	plt.show()
 	return sigma1, sigma2
 
-# sys.argv is a list of the command line flags
-# to use a data file, specify with a flag
 
-if len(sys.argv) > 1:
-	q_squared = []
-	energies = []
-	thetas = []
-	cross_sections = []
-	uncertainties = []
-
-	file = sys.argv[1]
-	try:
-		with open(file, 'rb') as f:
-			data = csv.reader(f, delimiter=" ")
-			# skip header row
-			next(data)
-			for row in data:
-				q_squared.append(float(row[0]))
-				energies.append(float(row[1]))
-				thetas.append(float(row[2]))
-				cross_sections.append(float(row[3]))
-				uncertainties.append(float(row[4]))
-	except IOError as e:
-		print(e)
-else:
-	q_squared = [4.0,4.0,4.0,4.0,4.0]
-	energies = [3.4, 3.956, 4.507, 5.507, 9.8]
-	thetas = [57.572, 43.707, 35.592, 26.823,13.248]
-	cross_sections = [1.297e-2, 2.77e-2, 4.929e-2, 1.023e-1, 6.18e-1]
-	uncertainties = [2.243e-4,4.407e-4,7.853e-4,1.370e-3,8.073e-3]
-
-# reset output file
-with open('out.csv','w') as out:
-	writer = csv.writer(out, delimiter=' ')
-	writer.writerow(['Q^2', 'G_E^2', 'σ_E', 'G_M^2', 'σ_M'])
-
-# print(q_squared)
-# print(energies)
-# print(thetas)
-# print(cross_sections)
-# print(uncertainties)
-
-# constants
-fsc = 0.00729735256
-mass = 0.938 # GeV
-
-# solve for ge^2 and gm^2 with least squares regression
-a = []
-b = []
+# only run this stuff if specifically executing this file
+# won't run if importing above functions
+if __name__ == '__main__':
 
 
-#keep track of previous q^2 to make separate estimates of form factors
+	# sys.argv is a list of the command line flags
+	# to use a data file, specify with a flag
+	if len(sys.argv) > 1:
+		q_squared = []
+		energies = []
+		thetas = []
+		cross_sections = []
+		uncertainties = []
 
-gm2_points = []
-ge2_points = []
-q2_vals = []
-
-q_squared, energies, thetas, cross_sections, total_errors = partition(q_squared, energies, thetas, cross_sections, uncertainties)
-for i in range(len(q_squared)):
-	if len(q_squared[i]) <= 1:
-		break
+		file = sys.argv[1]
+		try:
+			with open(file, 'rb') as f:
+				data = csv.reader(f, delimiter=" ")
+				# skip header row
+				next(data)
+				for row in data:
+					q_squared.append(float(row[0]))
+					energies.append(float(row[1]))
+					thetas.append(float(row[2]))
+					cross_sections.append(float(row[3]))
+					uncertainties.append(float(row[4]))
+		except IOError as e:
+			print(e)
 	else:
-		for j in range(len(q_squared[i])):
-			q2 = q_squared[i][j]
-			energy = energies[i][j]
-			theta = thetas[i][j]
-			cross_section = cross_sections[i][j]
-			# total_errors.append(uncertainties[i][j])
+		q_squared = [4.0,4.0,4.0,4.0,4.0]
+		energies = [3.4, 3.956, 4.507, 5.507, 9.8]
+		thetas = [57.572, 43.707, 35.592, 26.823,13.248]
+		cross_sections = [1.297e-2, 2.77e-2, 4.929e-2, 1.023e-1, 6.18e-1]
+		uncertainties = [2.243e-4,4.407e-4,7.853e-4,1.370e-3,8.073e-3]
 
-			result = rosenbluth(q2, energy, theta, cross_section)
-			tau = result[1]
+	# reset output file
+	with open('out.csv','w') as out:
+		writer = csv.writer(out, delimiter=' ')
+		writer.writerow(['Q^2', 'G_E^2', 'σ_E', 'G_M^2', 'σ_M'])
 
-			a.append(result[0])
-			b.append(result[2])
+	# print(q_squared)
+	# print(energies)
+	# print(thetas)
+	# print(cross_sections)
+	# print(uncertainties)
 
-		regression = form_factors(a, b)
-		ge_squared = regression[0]
-		gm_squared = regression[1]/tau
-		print("Q^2 = " + str(q2))
-		print("G_e^2 = " + str(ge_squared))
-		print("G_m^2 = " + str(gm_squared))
-		print
-		# Monte Carlo simulation
-		# sample from normal distribution around reduced cross section and epsilon
-		eps = []
-		red = []
-		ge2 = []
-		gm2 = []
-		samples = 10000
-		for j in range(len(a)):
-			#epsilon
-			s = [a[j]]*samples
-			eps.append(s)
-			#reduced
-			s = random.normal(b[j], total_errors[i][j]/100, samples)
-			red.append(s)
-		eps = array(eps)
-		red = array(red)
+	# solve for ge^2 and gm^2 with least squares regression
+	a = []
+	b = []
 
 
-		for j in range(samples):
-			# accessing i-th column of matrix
-			eps_samples = eps[:,j]
-			red_samples = red[:,j]
-			res = form_factors(eps_samples, red_samples)
-			#negative slope of fit becomes 0
+	#keep track of previous q^2 to make separate estimates of form factors
 
-			if res[0] < 0:
-				ge2.append(res[0])
-				pass
-			else:
+	gm2_points = []
+	ge2_points = []
+	q2_vals = []
 
-				#divide out tau
-				ge2.append(res[0])
-			gm2.append(res[1]/tau)
+	q_squared, energies, thetas, cross_sections, total_errors = partition(q_squared, energies, thetas, cross_sections, uncertainties)
+	for i in range(len(q_squared)):
+		if len(q_squared[i]) <= 1:
+			break
+		else:
+			for j in range(len(q_squared[i])):
+				q2 = q_squared[i][j]
+				energy = energies[i][j]
+				theta = thetas[i][j]
+				cross_section = cross_sections[i][j]
+				# total_errors.append(uncertainties[i][j])
 
-		a = []
-		b = []
-		# ge2_points.append(ge2)
-		# gm2_points.append(gm2)
-		# q2_vals.append(q2)
-		sigma1, sigma2 = plot_form_factors(ge2, gm2, q2)
-		with open('out.csv', 'a') as out:
-			writer = csv.writer(out, delimiter=' ')
-			writer.writerow([q2, ge_squared, sigma1, gm_squared, sigma2])
+				result = rosenbluth(q2, energy, theta, cross_section)
+				tau = result[1]
+
+				a.append(result[0])
+				b.append(result[2])
+
+			regression = form_factors(a, b)
+			ge_squared = regression[0]
+			gm_squared = regression[1]/tau
+			print("Q^2 = " + str(q2))
+			print("G_e^2 = " + str(ge_squared))
+			print("G_m^2 = " + str(gm_squared))
+			print
+			# Monte Carlo simulation
+			# sample from normal distribution around reduced cross section and epsilon
+			eps = []
+			red = []
+			ge2 = []
+			gm2 = []
+			samples = 10000
+			for j in range(len(a)):
+				#epsilon
+				s = [a[j]]*samples
+				eps.append(s)
+				#reduced
+				s = random.normal(b[j], total_errors[i][j]/100, samples)
+				red.append(s)
+			eps = array(eps)
+			red = array(red)
+
+
+			for j in range(samples):
+				# accessing i-th column of matrix
+				eps_samples = eps[:,j]
+				red_samples = red[:,j]
+				res = form_factors(eps_samples, red_samples)
+				#negative slope of fit becomes 0
+
+				if res[0] < 0:
+					ge2.append(res[0])
+					pass
+				else:
+
+					#divide out tau
+					ge2.append(res[0])
+				gm2.append(res[1]/tau)
+
+			a = []
+			b = []
+			# ge2_points.append(ge2)
+			# gm2_points.append(gm2)
+			# q2_vals.append(q2)
+			sigma1, sigma2 = plot_form_factors(ge2, gm2, q2)
+			with open('out.csv', 'a') as out:
+				writer = csv.writer(out, delimiter=' ')
+				writer.writerow([q2, ge_squared, sigma1, gm_squared, sigma2])
 
