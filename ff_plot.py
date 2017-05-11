d7d49b52ffa37e435d14845a84b034f3fceccfdc
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2017-05-11 17:58:08 -0400

    [ADD] JLab Hall A (Super Rosenbluth) data, form factors, and plots

--- ff_plot.py
+++ ff_plot.py
@@ -5,7 +5,7 @@ import sys, csv
 from os import path
 from rosenbluth import pmm, dipole_form_factor
 from numpy import arange
-
+from monte_carlo_ratio_errors import calc_ff_ratio_err
 ge = []
 ge_error = []
 gm = []
@@ -21,7 +21,7 @@ def marker():
 
 	"""
 
-	syms = ['s', 'o', '^']
+	syms = ['o', '^', 's']
 	while True:
 		for s in syms:
 			yield s
@@ -70,7 +70,10 @@ if len(sys.argv) > 1:
 		ge_error.append([])
 		gm_error.append([])
 		q2.append([])
-		file_path = path.relpath("Figures/"+files[i])
+		if "/" not in files[i]:
+			file_path = path.relpath("Figures/"+files[i])
+		else:
+			file_path = path.relpath(files[i])
 		try:
 
 			with open(file_path, 'r') as f:
@@ -95,24 +98,29 @@ if len(sys.argv) > 1:
 	rc('font',**{'family':'serif'})
 
 	# plot G_E/G_D
-	plt.figure(figsize=(6,5))
+	plt.figure(figsize=(12,10))
 	ax = plt.subplot(111)
 	ax.set_xlabel(r'$Q^2$', fontsize=30)
 	ax.set_ylabel(r'$G_E^2/G_{SD}^2$', fontsize=30)
 	ax.set_xlim([0,8.0])
 	plt.xticks(fontsize=20)
 	plt.yticks(fontsize=20)
-	# mark = marker()
+	mark = marker()
 	c = color()
 	fill = True
 	c1 = next(c)
+	m1 = next(mark)
 	for i in range(len(q2)):
-		# c1 = next(c)
+
 		if fill:
-			plt.errorbar([j+i/20 for j in q2[i]], ge[i], yerr=ge_error[i], fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2, label="RC")
+			plt.errorbar([j+i/20 for j in q2[i]], ge[i], yerr=ge_error[i], fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2)
 		else:
-			plt.errorbar([j+i/20 for j in q2[i]], ge[i], yerr=ge_error[i], fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2, label='No RC')
+			plt.errorbar([j+i/20 for j in q2[i]], ge[i], yerr=ge_error[i], fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2)
+			c1 = next(c)
+			m1 = next(mark)
+			
 		fill = not fill
+		# c1 = next(c)
 
 	# plot a fit line where G_E = (1+x/0.662)^{-2} and G_D = (1+x/0.71)^{-2}
 	x = arange(0., 8., 0.1)
@@ -120,21 +128,22 @@ if len(sys.argv) > 1:
 	plt.plot(x, y, '-g', label='Carlson-Griffioen')
 
 	# add a legend
-	if len(files) > 1:
-		handles, labels = ax.get_legend_handles_labels()
-		# handles.append(fit_curve)
-		print(handles, labels)
-		# handles = [h[0] for h in handles[0]]
-		ax.legend(handles, labels, loc='upper left', numpoints=1, fontsize=20)
+	# if len(files) > 1:
+	# 	handles, labels = ax.get_legend_handles_labels()
+	# 	# handles.append(fit_curve)
+	# 	print(handles, labels)
+	# 	# handles = [h[0] for h in handles[0]]
+	# 	ax.legend(handles, labels, loc='upper left', numpoints=1, fontsize=20)
 
 	# shade unphysical region
-	ax.set_ylim(-0.5, ax.get_ylim()[1])
+	lim = ax.get_ylim()
 	if ax.get_ylim()[0] < 0:
+		
 		plt.axhspan(ax.get_ylim()[0], 0, facecolor='gray', alpha=0.25)
-	
-	file_names = [i.split("+")[1][:-4] for i in files]
-	plt.savefig("Figures/Form_Factors/ge-gd-" + "&".join(file_names) + ".png", bbox_inches="tight")
-	# plt.show()
+	ax.set_ylim(lim)
+	# file_names = [i.split("+")[1][:-4] for i in files]
+	# plt.savefig("Figures/Form_Factors/ge-gd-" + "&".join(file_names) + ".png", bbox_inches="tight")
+	plt.show()
 
 	# plot G_M/mu G_D
 	plt.figure(figsize=(12,10))
@@ -148,13 +157,17 @@ if len(sys.argv) > 1:
 	c = color()
 	fill = True
 	c1 = next(c)
+	m1 = next(mark)
 	for i in range(len(q2)):
-		# c1 = next(c)
 		if fill:
-			plt.errorbar([j+i/20 for j in q2[i]], [gm[i][j]/pmm**2 for j in range(len(gm[i]))], yerr=[gm_error[i][j]/pmm**2 for j in range(len(gm_error[i]))], fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2)
+			plt.errorbar([j+i/20 for j in q2[i]], [gm[i][j]/pmm**2 for j in range(len(gm[i]))], yerr=[gm_error[i][j]/pmm**2 for j in range(len(gm_error[i]))], fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2)
 		else:
-			plt.errorbar([j+i/20 for j in q2[i]], [gm[i][j]/pmm**2 for j in range(len(gm[i]))], yerr=[gm_error[i][j]/pmm**2 for j in range(len(gm_error[i]))], fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2)
-		fill=not fill
+			plt.errorbar([j+i/20 for j in q2[i]], [gm[i][j]/pmm**2 for j in range(len(gm[i]))], yerr=[gm_error[i][j]/pmm**2 for j in range(len(gm_error[i]))], fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2)
+			c1 = next(c)
+			m1 = next(mark)
+			
+		fill = not fill
+		# c1 = next(c)
 
 	# plot a fit line where G_M = mu
 	x = arange(0., 8., 0.1)
@@ -163,11 +176,12 @@ if len(sys.argv) > 1:
 
 	# shade unphysical region
 	if ax.get_ylim()[0] < 0:
+		
 		plt.axhspan(ax.get_ylim()[0], 0, facecolor='gray', alpha=0.25)
 
-	file_names = [i.split("+")[1][:-4] for i in files]
-	plt.savefig("Figures/Form_Factors/gm-gd-" + "&".join(file_names) + ".png", bbox_inches="tight")
-	# plt.show()
+	# file_names = [i.split("+",maxsplit=1)[1][:-4] for i in files]
+	# plt.savefig("Figures/Form_Factors/gm-gd-" + "&".join(file_names) + ".png", bbox_inches="tight")
+	plt.show()
 
 	# plot mu G_E/G_M
 	plt.figure(figsize=(12,10))
@@ -181,15 +195,21 @@ if len(sys.argv) > 1:
 	c = color()
 	c1 = next(c)
 	fill = True
+	m1 = next(mark)
 	for i in range(len(q2)):
-		# c1 = next(c)
 		# recalculate error for a product/ratio of variables (see http://www.utm.edu/staff/cerkal/Lect4.html)
-		ratio_err = [pmm**2*ge[i][j]/gm[i][j] * ((ge_error[i][j]/ge[i][j])**2 + (gm_error[i][j]/gm[i][j])**2)**0.5 for j in range(len(ge[i]))]
+		ratio_err = [pmm**2 * ge[i][j] / gm[i][j] * ( (ge_error[i][j] / ge[i][j]) ** 2 + (gm_error[i][j] / gm[i][j]) ** 2) ** 0.5 for j in range(len(ge[i]))]
+		# ratio_err = [calc_ff_ratio_err(ge[i][j], ge_error[i][j], gm[i][j], gm_error[i][j]) for j in range(len(ge[i]))]
+		# print(ratio_err, ratio_err2)
 		if fill:
-			plt.errorbar([j+i/20 for j in q2[i]], [pmm**2*ge[i][j]/gm[i][j] for j in range(len(ge[i]))], yerr=ratio_err, fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2)
+			plt.errorbar([j+i/20 for j in q2[i]], [pmm**2*ge[i][j]/gm[i][j] for j in range(len(ge[i]))], yerr=ratio_err, fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor=c1, markeredgecolor=c1, markeredgewidth=2)
 		else:
-			plt.errorbar([j+i/20 for j in q2[i]], [pmm**2*ge[i][j]/gm[i][j] for j in range(len(ge[i]))], yerr=ratio_err, fmt='o', elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2)
+			plt.errorbar([j+i/20 for j in q2[i]], [pmm**2*ge[i][j]/gm[i][j] for j in range(len(ge[i]))], yerr=ratio_err, fmt=m1, elinewidth=2, markersize=10, ecolor=c1, markerfacecolor='none', markeredgecolor=c1, markeredgewidth=2)
+			c1 = next(c)
+			m1 = next(mark)
+			
 		fill = not fill
+		# c1 = next(c)
 
 	# plot a fit line where mu G_E/G_M = (1-x/8.02)
 	x = arange(0., 8., 0.1)
@@ -200,7 +220,7 @@ if len(sys.argv) > 1:
 	if ax.get_ylim()[0] < 0:
 		plt.axhspan(ax.get_ylim()[0], 0, facecolor='gray', alpha=0.25)
 
-	file_names = [i.split("+")[1][:-4] for i in files]
-	plt.savefig("Figures/Form_Factors/ge-gm-" + "&".join(file_names) + ".png", bbox_inches="tight")
-	# plt.show()
+	# file_names = [i.split("+")[1][:-4] for i in files]
+	# plt.savefig("Figures/Form_Factors/ge-gm-" + "&".join(file_names) + ".png", bbox_inches="tight")
+	plt.show()
 	
